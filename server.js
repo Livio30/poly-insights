d2446cd21606647286642b1e731003b33f00330f
Chaitanyacodes <chaitanya.chanekar2292@gmail.com>
2021-11-13 12:47:06 +0530

    build: server-setup

--- server.js
+++ server.js
@@ -4,3 +4,25 @@ const cors = require('cors');
 
 dotenv.config();
 const app = express();
+
+// cross origin requests
+app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      credentials: true,
+    })
+  );
+
+if(process.env.NODE_ENV === 'production'){
+// set static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+}
+
+// port connection
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
