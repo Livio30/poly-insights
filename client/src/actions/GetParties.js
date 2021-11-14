c1c64badcc26a0316df996a2ff0ebc45ed54f623
Priya deelip Gaude <94193274+wandamax0@users.noreply.github.com>
2021-11-14 11:09:30 +0530

    c

--- client/src/actions/GetParties.js
+++ client/src/actions/GetParties.js
@@ -4,17 +4,18 @@ import partynames from "../Tempdata/partynames";
   export const getparties = () => async (dispatch) => {
     dispatch({ type: GET_PARTIES_REQUEST });
     try {
-      const res = await fetch("/api/party-list", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await res.json();
+    //   const res = await fetch("/getblog", {
+     //    method: "GET",
+   //      headers: {
+  //         "Content-Type": "application/json",
+  //      },
+  //   });
+  //     const data = await res.json();
   //     console.log(`post data:${data}`);
-  // console.log("Sending ");
-  // console.log(partynames);
-      dispatch({ type: GET_PARTIES_SUCCESS, payload: data });
+  //console.log("Sending ");
+  //console.log(partynames);
+    console.log(data);
+      dispatch({ type: GET_PARTIES_SUCCESS, payload: partynames });
     } catch (e) {
       dispatch({ type: GET_PARTIES_FAIL, payload: e.message });
     }
